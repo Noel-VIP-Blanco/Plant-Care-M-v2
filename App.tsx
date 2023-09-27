88d350892088bf5bf512b753ad3e183827b39bde
Noel Blanco <noel.vip.blanco@gmail.com>
2023-09-27 14:56:36 +0700

    Initial Commit for ForgotPasswordScreen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { PaperProvider } from 'react-native-paper';
-import Index from '@root/app/Index';
+import { StatusBar } from "expo-status-bar";
+import { PaperProvider } from "react-native-paper";
+import Index from "@root/app/Index";
 
 export default function App() {
   return (
-      <PaperProvider>
-        <StatusBar style="auto" />
-        <Index/>
-      </PaperProvider>
+    <PaperProvider>
+      <StatusBar style="auto" />
+      <Index />
+    </PaperProvider>
   );
 }
