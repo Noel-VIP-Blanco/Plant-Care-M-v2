87eda645cda3815926639d77631359da5bfc7670
Decrypted707 <michaelangeloduran38@gmail.com>
2023-10-15 10:37:25 +0800

    Initial commit ffor AddAwsApiCalls

--- app/interface/DataProps/ContainerItemProps.ts
+++ app/interface/DataProps/ContainerItemProps.ts
@@ -3,3 +3,25 @@ export type ContainerItemProps = {
   contName: string;
   arduinoBoardId: string;
 };
+
+export type ContainerProps = {
+  id: number;
+  name: string;
+  arduinoBoardDto: {
+    id: number;
+    name: string;
+    status: string;
+    sensorMappings: any[] | null; // Define the appropriate type for sensorMappings based on your data structure
+  };
+  plantDto: {
+    id: number;
+    name: string;
+    maximumEc: number;
+    maximumPh: number;
+    minimumEc: number;
+    minimumPh: number;
+    daysToMaturity: string;
+    farmId: number;
+  };
+  farmId: number;
+};
